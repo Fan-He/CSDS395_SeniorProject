cadf7b6f2c0079e8bccf2ea6826dfe4a25103f02
Xiaofan He <xxh395@case.edu>
2022-11-30 17:46:18 -0500

    firestore linked to homepage

--- meow-woof/src/Home.js
+++ meow-woof/src/Home.js
@@ -12,7 +12,7 @@ const querySnapshot = await getDocs(collection(db, "product"));
 const products = [];
 querySnapshot.forEach((doc) => {
   // doc.data() is never undefined for query doc snapshots
-  console.log(doc.id, " => ", doc.data());
+//   console.log(doc.id, " => ", doc.data());
   products.push(doc.data());
 });
 
