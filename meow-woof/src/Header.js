c8e5111b26f201437702e7393b091077db5cbe81
Xiaofan He <xxh395@case.edu>
2022-11-29 23:58:54 -0500

    reformat homepage and created search page, header keeped names

--- meow-woof/src/Header.js
+++ meow-woof/src/Header.js
@@ -1,21 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState, useRef}from 'react'
 import './Header.css'
 import logo from './images/logo.png'
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketTwoToneIcon from '@mui/icons-material/ShoppingBasketTwoTone';
 import { Link } from "react-router-dom"
 import { useStateValue } from './StateProvider';
+import algoliasearch from 'algoliasearch/lite';
+import {
+    InstantSearch,
+    Configure,
+    Hits,
+    SearchBox,
+    Panel,
+    RefinementList,
+    Pagination,
+    Highlight,
+  } from 'react-instantsearch-dom';
+import Product1 from './Product1';
+
+const Hit = ({hit})=>
+    <div className = "hit">
+        <div className = "result">
+            <Product1
+                id={hit.objectID} 
+                title={hit.title}
+                image={hit.image_url}
+                price={hit.price} 
+                rating={hit.rating}
+                detail={hit.detail}
+                />
+        </div>
+    </div>
+
+
+    
+const algoliaClient = algoliasearch("ICV2YF7XAB","802af3124223d9f3a2c0f38c579b7763");
+const searchClient = {
+    ...algoliaClient,
+    search(requests) {
+      return algoliaClient.search(requests);
+    },
+  };
+
 
 function Header() {
-
+    const [showHits, setShowHits] = useState(false);
     const [{basket}, dispatch] = useStateValue();
 
     const mapState = (state) => ({
         currentUser: state.user.currentUser
     });
 
-    return (
+    let name = localStorage.getItem("name");
 
+    return (
         <div className='header'>
             <Link to="/">
                 <img className="header_logo" src={logo} alt=""></img>
@@ -32,7 +70,7 @@ function Header() {
                 <Link to="/login">
                     <div className="header_option">
                         <span className='header_optionLineOne'>
-                            Hello Guest
+                            Hello {name}
                         </span>
                         <span className='header_optionLineTwo'>
                             Sign In
