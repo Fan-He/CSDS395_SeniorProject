59b716c0d105a453e47b35ae334ad5716a68de74
Xiaofan He <xxh395@case.edu>
2022-11-30 00:09:09 -0500

    formatting header

--- meow-woof/src/Header.js
+++ meow-woof/src/Header.js
@@ -101,7 +101,7 @@ function Header() {
                 </div>
 
                 <Link to="/checkout">
-                    <div className='header_optionBasket111'>
+                    <div className='header_optionBasket'>
                         <ShoppingBasketTwoToneIcon/>
                         {/* two class names */}
                         <span className="header_optionLineTwo header_basketCount">{basket?.length}</span>
