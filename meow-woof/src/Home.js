3f99348ecafc31973ff99462a777b47f5a809229
Xiaofan He <xxh395@case.edu>
2022-12-01 23:50:16 -0500

    product components

--- meow-woof/src/Home.js
+++ meow-woof/src/Home.js
@@ -6,9 +6,11 @@ import Product from './Product'
 import { Link } from "react-router-dom"
 import {QuerySnapshot} from "firebase/firestore";
 import {db} from "./firebase-config";
+import scroller from "./virtual-and-infinite"
 
 const querySnapshot = await getDocs(collection(db, "product"));
 const products = [];
+const productComponents = [];
 querySnapshot.forEach((doc) => {
   // doc.data() is never undefined for query doc snapshots
 //   console.log(doc.id, " => ", doc.data());
@@ -19,7 +21,9 @@ querySnapshot.forEach((doc) => {
   products.push(p);
 });
 
-
+productComponents.push(products.map(data => (
+  <Product title = {data.Title} price = {data.Price} rating = {data.Rating} image = {data.Image_url} detail={data.Detail}/>
+)));
 
 function Home() {
 
@@ -29,9 +33,11 @@ function Home() {
         <div className='home_container'>
             <img className='home_image' src={Cats} alt=""></img>
             <div className='home_row'>
-                {products.map(data => (
+                {/* {products.map(data => (
                     <Product title = {data.Title} price = {data.Price} rating = {data.Rating} image = {data.Image_url} detail={data.Detail}/>
-                ))}
+                ))} */}
+                {/* <scroller listItems={productComponents} height={30}/> */}
+                {productComponents}
             </div>
 
             
