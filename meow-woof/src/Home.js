f40b95afc6a3a77610baa1cd7eddd66fe2361160
Xiaofan He <xxh395@case.edu>
2022-12-02 00:49:20 -0500

    scrolling homepage implemented

--- meow-woof/src/Home.js
+++ meow-woof/src/Home.js
@@ -6,11 +6,11 @@ import Product from './Product'
 import { Link } from "react-router-dom"
 import {QuerySnapshot} from "firebase/firestore";
 import {db} from "./firebase-config";
-import scroller from "./virtual-and-infinite"
+
 
 const querySnapshot = await getDocs(collection(db, "product"));
 const products = [];
-const productComponents = [];
+let offset = 30;
 querySnapshot.forEach((doc) => {
   // doc.data() is never undefined for query doc snapshots
 //   console.log(doc.id, " => ", doc.data());
@@ -21,23 +21,52 @@ querySnapshot.forEach((doc) => {
   products.push(p);
 });
 
-productComponents.push(products.map(data => (
-  <Product title = {data.Title} price = {data.Price} rating = {data.Rating} image = {data.Image_url} detail={data.Detail}/>
-)));
+
+// const productsList = products.slice(0, 30);
+
+
+
+
+
+
+
 
 function Home() {
 
+    const [productList, setProductList] = useState([]);
+
+    useEffect(() => {
+      window.addEventListener('scroll', handleScroll);
+      setProductList(products.slice(0, 30));
+    }, []);
+    
+    const handleScroll = (e) => {
+      //console.log('handle scrolled')
+      if(
+        window.innerHeight + e.target.documentElement.scrollTop + 1 > e.target.documentElement.scrollHeight
+      ){
+        
+        loadMoreProducts();
+      }
+    };
+
+    const loadMoreProducts = () => {
+      const newProducts = products.slice(offset, offset + 15);
+      setProductList(oldProducts => [...oldProducts, ...newProducts]);
+      //productsList.push(...newProducts);
+      offset += 15;
+    }
 
     return (
     <div className='home'>
         <div className='home_container'>
             <img className='home_image' src={Cats} alt=""></img>
             <div className='home_row'>
-                {/* {products.map(data => (
+                {productList.map(data => (
                     <Product title = {data.Title} price = {data.Price} rating = {data.Rating} image = {data.Image_url} detail={data.Detail}/>
-                ))} */}
+                ))}
                 {/* <scroller listItems={productComponents} height={30}/> */}
-                {productComponents}
+                {/* {productComponents} */}
             </div>
 
             
